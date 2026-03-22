260a5c687fc6b2dc24845b07bb7719fded18584c
noahdc06 <noahdc06@my.yorku.ca>
2026-03-22 15:16:57 -0400

    Update GameMain.java

--- LegendsOfSwordAndWand_Project/src/com/legends/main/GameMain.java
+++ LegendsOfSwordAndWand_Project/src/com/legends/main/GameMain.java
@@ -1,28 +1,10 @@
 package com.legends.main;
 
-import com.legends.model.Profile;
-import com.legends.database.UserDAO;
-import com.legends.service.PvECampaign;
+import com.legends.ui.HomePage;
 
 public class GameMain {
 
     public static void main(String[] args) {
-
-        try {
-            UserDAO dao = new UserDAO();
-
-            Profile profile = new Profile("player1", "1234");
-            dao.createUser(profile);
-
-            Profile loggedIn = dao.login("player1", "1234");
-
-            if (loggedIn != null) {
-                PvECampaign campaign = new PvECampaign(loggedIn);
-                campaign.startCampaign();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new HomePage();
     }
 }
