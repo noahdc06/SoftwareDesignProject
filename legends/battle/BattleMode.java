d7ceeb1173518ab559e188527bba6cf85512a535
noahdc06 <noahdc06@my.yorku.ca>
2026-04-03 15:08:00 -0400

    Delete legends directory

--- legends/battle/BattleMode.java
+++
@@ -1,6 +0,0 @@
-package com.legends.battle;
-//Enum for Battle Modes
-public enum BattleMode {
-    PVE,
-    PVP
-}
--- legends/battle/Command.java
+++
@@ -1,8 +0,0 @@
-package com.legends.battle;
-//Types of commands
-public enum Command {
-    ATTACK,
-    RECOVER,
-    SPELL_1,
-    SPELL_2
-}
